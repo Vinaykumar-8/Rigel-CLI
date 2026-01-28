01901c80a118c66d8289479ca4b629cb79a0061c
Vinaykumar-8 <vinay17022005@gmail.com>
2026-01-28 13:43:51 +0530

    Refactor output handling and variable names

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
+const handleFunction =()=>{
+  send(output);
+};
+
 const send =(callback)=>{
   chat=document.getElementById('textA');
   input=document.getElementById('input');
 
-  if(input.value.trim()!=""){
-    let value=input.value;
+  if(input.value.trim()!==""){
+    let val=input.value;
     input.value="";
     callback(val);
-    chat.value=`User Input: \n ${chat.value}`;
+    chat.value=`User Input: \n ${val}`;
     chat.scrollTop=chat.scrollHeight;
   }
 };
 
 const output =(val)=>{
   val=val.toUpperCase();
-  let output="";
+  let out="";
   chat=document.getElementById('textA');
   
-  if(val.value=="HELLO"){
-    output="Hello User Welcome To RIGEL_CLI";
+  if(val==="HELLO"){
+    out="Hello User Welcome To RIGEL_CLI";
   }
-  else if(val.value=="HTML"){
-    output=`HTML stands for HyperText Markup Language
+  else if(val==="HTML"){
+    out=`HTML stands for HyperText Markup Language
     It is used to define the structure of the web pages
     and its components. We can even use it define the
     dynamic web pages when used with CSS and Javascript.
@@ -29,12 +33,12 @@ const output =(val)=>{
     html file is <!DOCTYPE html> and it can be saved using
     the .html file extension.`
   }
-  else if(val.value=="CLEAR"){
+  else if(val==="CLEAR"){
     chat.value=`RIGEL HAS CLEANED THE AREA
     ------------------------------`
   }
-  else if(val.value=="HELP"){
-    output=`Here are sone of the keywords you can use:
+  else if(val==="HELP"){
+    out=`Here are sone of the keywords you can use:
     1.Clear (To clear the chat area).
     2.HTML (To get info regarding HTML).
     3.CSS (To get info regarding CSS).
@@ -42,8 +46,8 @@ const output =(val)=>{
     5.Explain (To get RIGEL'S introduction).`
   }
   else{
-    output="Sorry, we do not have information regarding that";
+    out="Sorry, we do not have information regarding that";
   }
   chat.value=`Response from RIGEL:
-  \n ${output.value} \n ============================`;
-}
+  \n ${out} \n ============================`;
+};
