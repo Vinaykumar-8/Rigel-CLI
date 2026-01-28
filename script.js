cd8042e4255db6fb521f19f93c4301740a3a7107
Vinaykumar-8 <vinay17022005@gmail.com>
2026-01-28 19:50:53 +0530

    Refactor chat input handling and output formatting

--- script.js
+++ script.js
@@ -3,39 +3,39 @@ const handleFunction =()=>{
 };
 
 const send =(callback)=>{
-  chat=document.getElementById('textA');
+  let chat=document.getElementById('textA');
   input=document.getElementById('input');
-
+  let val=input.value;
+  
   if(input.value.trim()!==""){
-    let val=input.value;
     input.value="";
-    callback(val);
     chat.value=`User Input: \n ${val}`;
     chat.scrollTop=chat.scrollHeight;
   }
+  callback(val);
 };
 
 const output =(val)=>{
   val=val.toUpperCase();
   let out="";
-  chat=document.getElementById('textA');
+  let chat=document.getElementById('textA');
   
   if(val==="HELLO"){
     out="Hello User Welcome To RIGEL_CLI";
   }
   else if(val==="HTML"){
-    out=`HTML stands for HyperText Markup Language
+    out=`* HTML stands for HyperText Markup Language.\n
     It is used to define the structure of the web pages
-    and its components. We can even use it define the
-    dynamic web pages when used with CSS and Javascript.
+    and its components.\n We can even use it define the
+    dynamic web pages when used with CSS and Javascript.\n
     It is a tag based language which uses pre-defined tags
-    for defining the components. The very first line of the
+    for defining the components.\n The very first line of the
     html file is <!DOCTYPE html> and it can be saved using
-    the .html file extension.`
+    the .html file extension.`;
   }
   else if(val==="CLEAR"){
     chat.value=`RIGEL HAS CLEANED THE AREA
-    ------------------------------`
+    ------------------------------`;
   }
   else if(val==="HELP"){
     out=`Here are sone of the keywords you can use:
@@ -43,11 +43,12 @@ const output =(val)=>{
     2.HTML (To get info regarding HTML).
     3.CSS (To get info regarding CSS).
     4.Hello (For Greetings).
-    5.Explain (To get RIGEL'S introduction).`
+    5.Explain (To get RIGEL'S introduction).`;
   }
   else{
     out="Sorry, we do not have information regarding that";
   }
-  chat.value=`Response from RIGEL:
+  chat.value+="\n";
+  chat.value+=`Response from RIGEL:
   \n ${out} \n ============================`;
 };
